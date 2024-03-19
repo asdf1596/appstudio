a4014bff3665de5d373ee31bf2c89279504667c3
asdf1596 <7082mc@naver.com>
2024-03-19 11:13:07 +0900

    asd

--- app/src/main/java/com/example/linearlayout/MainActivity.java
+++ app/src/main/java/com/example/linearlayout/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.linearlayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear_2_1);
+        setContentView(R.layout.school_info);
+
+        Button preViewButton = findViewById(R.id.prevButton);
+        Button nextButton = findViewById(R.id.nextButton);
+        //1. 버튼에 클릭 이벤트 리스너를 등록하는 방법
+        preViewButton.setOnClickListener(this);
+        //2.버튼에 익명클래스로 이벤트 리스너 처리
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("익명클래스로 이벤트 처리", "nextButton 클릭");
+                Toast.makeText(MainActivity.this, "nextButton 클릭", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        Log.d("버튼에 이벤트 리스너 등록", "preViewButton 클릭");
+        Toast.makeText(this, "preViewButton 버튼 클릭", Toast.LENGTH_SHORT).show();
     }
 }
