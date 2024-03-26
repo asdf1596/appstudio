d201aea63eb8d82dcb16c0aad4e4c445cf70a2cb
asdf1596 <7082mc@naver.com>
2024-03-26 11:28:55 +0900

    asdf

--- app/src/main/java/com/example/linearlayout/MainActivity.java
+++ app/src/main/java/com/example/linearlayout/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -17,14 +18,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button preViewButton = findViewById(R.id.prevButton);
         Button nextButton = findViewById(R.id.nextButton);
+        EditText idEditText = findViewById(R.id.idEditText);
         //1. 버튼에 클릭 이벤트 리스너를 등록하는 방법
         preViewButton.setOnClickListener(this);
         //2.버튼에 익명클래스로 이벤트 리스너 처리
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("익명클래스로 이벤트 처리", "nextButton 클릭");
-                Toast.makeText(MainActivity.this, "nextButton 클릭", Toast.LENGTH_SHORT).show();
+
+                String id = idEditText.getText().toString();
+                if(id.length() > 5){
+                    Toast.makeText(MainActivity.this, id+"님 반갑습니다.",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this,"아이디를 다시 입력하십시오.",Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
     }
